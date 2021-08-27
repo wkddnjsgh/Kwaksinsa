1285726b96ece9645c8f516bf51080a5a9fa9046
ABooop <luca4759@gmail.com>
2021-08-27 10:42:51 +0900

    Initial commit

---
+++ src/main/java/com/kwaksamo/kwaksinsa/KwaksinsaApplication.java
@@ -0,0 +1,13 @@
+package com.kwaksamo.kwaksinsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwaksinsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KwaksinsaApplication.class, args);
+    }
+
+}
